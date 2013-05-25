f326cdd6617511e8e3b0f26e9da9cb50792ab0b2
Claas <claas.augner@fu-berlin.de>
2013-05-25 21:26:34 +0300

    Update db_lang.php
    typo ($lang['db_del_must_use_where']) + improve

--- db_lang.php
+++ db_lang.php
@@ -1,16 +1,16 @@
 <?php
 
-$lang['db_invalid_connection_str'] = 'Die Datenbankkonfiguration konnte nicht f&uuml;r die angegebene Verbindung ermittelt werden.';
-$lang['db_unable_to_connect'] = 'Mit den angegebenen Verbindungsdaten konnte keine Verbindung zur Datenbank hergestellt werden.';
-$lang['db_unable_to_select'] = 'Die Verbindung zur Datenbank %s ist fehlgeschlagen.';
+$lang['db_invalid_connection_str'] = 'Die Datenbankkonfiguration konnte f&uuml;r die angegebene Verbindung nicht ermittelt werden.';
+$lang['db_unable_to_connect'] = 'Die Verbindung zur Datenbank konnte mit der angegebenen Konfiguration nicht hergestellt werden.';
+$lang['db_unable_to_select'] = 'Die Datenbank %s konnte nicht ausgew&auml;hlt werden.';
 $lang['db_unable_to_create'] = 'Die Datenbank %s konnte nicht erstellt werden.';
-$lang['db_invalid_query'] = 'Die angegebene Abfrage ist nicht g&uuml;ltig.';
-$lang['db_must_set_table'] = 'Es muss eine Datenbanktabelle f&uuml;r die Abfrage angegeben werden.';
-$lang['db_must_use_set'] = 'Der Befehl "SET" muss in der Aktualisierungs-Abfrage enthalten sein.';
-$lang['db_must_use_index'] = 'Es muss ein passender Index für Batch Updates angegeben werden.';
-$lang['db_batch_missing_index'] = 'Einer oder mehreren gegebenen Reihen für das Batch Update fehlt der angebene Index.';
+$lang['db_invalid_query'] = 'Die angegebene Abfrage ist ung&uuml;ltig.';
+$lang['db_must_set_table'] = 'Diese Abfrage erfordert die Angabe einer Datenbanktabelle.';
+$lang['db_must_use_set'] = 'Der Befehl "set" muss verwendet werden, um einen Eintrag zu aktualisieren.';
+$lang['db_must_use_index'] = 'Batch-Updates erfordern einen passenden.';
+$lang['db_batch_missing_index'] = 'Einem oder mehreren Eintr&auml;gen f&uuml;r das Batch-Update fehlt ein Index.';
 $lang['db_must_use_where'] = 'Aktualisierungen von Datens&auml;tzen erfordern eine "WHERE"-Klausel.';
-$lang['db_del_must_use_where'] = 'Das Laouml;schen von Datens&auml;tzen erfordert eine "WHERE"-Klausel.';
+$lang['db_del_must_use_where'] = 'Das L&ouml;schen von Datens&auml;tzen erfordert eine "WHERE"-Klausel.';
 $lang['db_field_param_missing'] = 'Der Name der Tabelle, aus der Daten abgefragt werden sollen, muss angegeben werden.';
 $lang['db_unsupported_function'] = 'Dieser Befehl wird von der verwendeten Datenbank nicht unterst&uuml;tzt.';
 $lang['db_transaction_failure'] = 'Die Transaktion ist fehlgeschlagen: Der vorherige Zustand wurde wiederhergestellt.';
@@ -18,11 +18,11 @@ $lang['db_unable_to_drop'] = 'Die Datenbank konnte nicht gelouml;scht werden.';
 $lang['db_unsuported_feature'] = 'Dieser Befehl wird von der verwendeten Datenbank nicht unterst&uuml;tzt.';
 $lang['db_unsuported_compression'] = 'Das verwendete Kompressions-Dateiformat wird von der Datenbank nicht unterst&uuml;tzt.';
 $lang['db_filepath_error'] = 'Die Ausgabe von Daten in den angegebenen Dateipfad ist fehlgeschlagen.';
-$lang['db_invalid_cache_path'] = 'Der Zwischenspeicher-Pfad (Cache) ist ung&uuml;ltig oder schreibgesch&uuml;tzt.';
-$lang['db_table_name_required'] = 'F&uuml;r diese Verarbeitung wird ein Tabellenname ben&ouml;tigt.';
-$lang['db_column_name_required'] = 'F&uuml;r diese Verarbeitung wird ein Spaltenname ben&ouml;tigt.';
-$lang['db_column_definition_required'] = 'F&uuml;r diese Verarbeitung wird eine Spaltenbeschreibung ben&ouml;tigt.';
-$lang['db_unable_to_set_charset'] = 'Unm&ouml;glich den Zeichensatz f&uuml;r die Client Verbindung zu setzen: %s';
+$lang['db_invalid_cache_path'] = 'Der Cache-Pfad ist ung&uuml;ltig oder schreibgesch&uuml;tzt.';
+$lang['db_table_name_required'] = 'Dieser Befehl erfordert die Angabe eines Tabellennamens.';
+$lang['db_column_name_required'] = 'Dieser Befehl erfordert die Angabe eines Spaltennamens.';
+$lang['db_column_definition_required'] = 'Dieser Befehl erfordert die Angabe einer Spaltendefinition.';
+$lang['db_unable_to_set_charset'] = 'Der Zeichensatz f&uuml;r die Datenbankverbindung konnte nicht festgelegt werden: %s';
 $lang['db_error_heading'] = 'Ein Datenbankfehler ist aufgetreten';
 
 /* End of file db_lang.php */
